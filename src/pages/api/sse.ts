30aefedd61364ec18768ccecae13f6afbacdb902
Velozity <zacaryzippy@gmail.com>
2023-03-06 02:30:45 +1100

    test

---
+++ src/pages/api/sse.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  res.writeHead(200, {
+    "Transfer-Encoding": "chunked",
+
+    "Content-Type": "text/event-stream",
+
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+    "Content-Encoding": "none",
+    "X-Accel-Buffering": "no",
+  });
+
+  res.write("data: 1\n\n");
+
+  let count = 2;
+  setTimeout(() => {
+    res.write(`data: ${count++}\n\n`);
+  }, 1000);
+};
+
+export default handler;
